c568725d7046d27fa354413c5fc3042c312abdaf
Alex Sklar <sklarfox@gmail.com>
2024-04-11 18:36:03 -0700

    feat(pieces): add piece1
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -35,18 +35,53 @@ export function Piece({ id, children, styles, synco }) {
 
   if (pieces[id - 1] == undefined) return
 
-  return (
-    <div
-      ref={setNodeRef}
-      className="w-24 aspect-square z-40"
-      style={{ ...style, ...styles, ...syncoPosition }}
-      {...listeners}
-      {...attributes}
-    >
-      <div className={'absolute w-[125px]'}>
-        <img src={pieces[id - 1]} alt="piece1" />
-      </div>
-      {children}
-    </div>
-  )
+  switch (id) {
+    case 1:
+      return (
+        <div
+          ref={setNodeRef}
+          className="w-24 aspect-square z-40"
+          style={{ ...style, ...styles, ...syncoPosition }}
+          {...listeners}
+          {...attributes}
+        >
+          <div className="absolute w-[121px] -translate-y-px">
+            <img src={pieces[id - 1]} alt="piece1" />
+          </div>
+          {children}
+        </div>
+      )
+    case 2:
+      return (
+        <div
+          ref={setNodeRef}
+          className="w-24 aspect-square z-40 overflow-visible"
+          style={{ ...style, ...styles, ...syncoPosition }}
+          {...listeners}
+          {...attributes}
+        >
+          <div className="absolute w-[120px] -translate-x-7">
+            <img src={pieces[id - 1]} alt="piece1" />
+          </div>
+          {children}
+        </div>
+      )
+    case 3:
+      return (
+        <div
+          ref={setNodeRef}
+          className="w-24 aspect-square z-40 overflow-visible"
+          style={{ ...style, ...styles, ...syncoPosition }}
+          {...listeners}
+          {...attributes}
+        >
+          <div className="absolute w-[100px] -translate-x-7 -translate-y-1">
+            <img src={pieces[id - 1]} alt="piece1" />
+          </div>
+          {children}
+        </div>
+      )
+    default:
+      return <div>hello</div>
+  }
 }
