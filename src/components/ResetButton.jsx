be55d795fbb8fa4c07f42b2b457e51e470ad00dc
Alex Sklar <sklarfox@gmail.com>
2024-04-15 12:19:49 -0700

    feat(reset): update css for button
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>

--- src/components/ResetButton.jsx
+++ src/components/ResetButton.jsx
@@ -1,12 +1,11 @@
 const ResetButton = ({ onClick }) => {
   return (
     <button
+      type="button"
       onClick={onClick}
-      className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-syncoGreen to-syncoBlue group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:outline-none"
+      className="text-white bg-gradient-to-br from-syncoGreen to-syncoBlue hover:bg-gradient-to-bl font-medium rounded-lg text-sm px-2.5 py-1 text-center me-2 mb-2"
     >
-      <span className="relative px-2.5 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-        Reset Puzzle
-      </span>
+      Reset Puzzle
     </button>
   )
 }
